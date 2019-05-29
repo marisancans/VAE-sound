632d94ab6a9a7556479f6b690e09f2209eff0e5c
maris.ancans <maris.ancans@applyit.lv>
2019-05-29 18:36:06 +0300

    Automatic deconv layers

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ args = parser.parse_args()
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('Using device:', DEVICE)
 
+batch_size = 2
+
+# Aviable sizes
+# [3 3] [7 7] [15 15] [31 31] [63 63] [127 127] [255 255] [511 511]
+WIDTH = 31
+HEIGHT = 31
 
 encoder = Encoder().to(DEVICE)
-decoder = Decoder().to(DEVICE)
-
-batch_size = 32
-WIDTH = 60
-HEIGHT = 64
-
+decoder = Decoder(HEIGHT, WIDTH, encoder.out_size).to(DEVICE)
 
 def rnd_color():
-    f = lambda : random.randint(0, 255)
+    f = lambda : random.random()
     return f(), f(), f()
 
 def create_image(id=0):
@@ -36,8 +37,8 @@ def create_image(id=0):
     img = np.ones((HEIGHT, WIDTH, 3), np.uint8)
     img[:] = r, 0, 0
 
-    # radius = random.randint(int(w/3), int(w/2))
-    cv2.circle(img, (int(HEIGHT/2), int(WIDTH/2)), int(r/5), (255, 0, 0), -1)
+    radius = random.randint(int(WIDTH/3), int(WIDTH/2))
+    cv2.circle(img, (int(HEIGHT/2), int(WIDTH/2)), 5, (255, 0, 0), -1)
     
     return img
 
@@ -59,6 +60,17 @@ params = list(encoder.parameters()) + list(decoder.parameters())
 optimizer = torch.optim.Adam(params, LR)
 loss_fn = torch.nn.MSELoss()
 
+def show(truth_t, pred_t):
+    img = torch.cat((truth_t, pred_t), dim=2)
+    img = img.permute(1, 2, 0)
+    img = img.cpu().detach().numpy()
+
+    img = truth_t
+
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
+
 for _ in range(100000):
     if args.debug:
         batch = make_batch_debug(batch_size)
@@ -69,8 +81,8 @@ for _ in range(100000):
     batch_t = batch_t.permute(0, 3, 1, 2) # (B, H, W, C) -->  (B, C, H, W)
 
     z_vector = encoder.forward(batch_t)
-    pred = decoder.forward(z_vector)
-    loss = loss_fn(pred, batch_t)
+    pred_t = decoder.forward(z_vector)
+    loss = loss_fn(pred_t, batch_t)
 
     optimizer.zero_grad()
     loss.backward()
@@ -78,13 +90,11 @@ for _ in range(100000):
 
     print(float(loss))
 
-   
+    truth = batch_t[0]
+    pred = pred_t[0]
 
+    show(truth, pred)
 
-
-    # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    # cv2.imshow('image',img)
-    # cv2.waitKey(250)
 
     x = 1
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import torchvision.models as models
+import torch.nn as nn
 
 class Encoder(torch.nn.Module):
     def __init__(self):    
@@ -27,17 +28,52 @@ class Encoder(torch.nn.Module):
 
     
 class Decoder(torch.nn.Module):
-    def __init__(self):    
+    def __init__(self, orig_height, orig_width, in_channels):    
         super().__init__()
 
-        self.deconv_1 = torch.nn.ConvTranspose2d(in_channels=20, out_channels=10, kernel_size=3)
-        self.deconv_2 = torch.nn.ConvTranspose2d(in_channels=10, out_channels=3, kernel_size=3)
+        h_out = 0
+        w_out = 0
+
+        h_in = 1
+        w_in = 1
+
+        stride = (2, 2)
+        kernel = (3, 3)
+
+        self.seq = nn.Sequential()
+        i = 0
+
+        while True:
+            h_out = stride[0] * (h_in - 1) + kernel[0]
+            w_out = stride[1] * (w_in - 1) + kernel[1]
+            h_in = h_out
+            w_in = w_out
+
+            is_last_layer = h_out == orig_height and w_out == orig_width
+
+            if is_last_layer:
+                out_channels = 3
+            else:
+                out_channels = int(in_channels // 2)
+
+            self.seq.add_module(f'deconv_{i}', nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel, stride=stride))
+            in_channels = out_channels
+
+            if not is_last_layer:
+                self.seq.add_module(f'relu_{i}', nn.ReLU())
+
+            print(f'layer: {i} | h_out: {h_out} | w_out: {h_out} | c_in: {in_channels} --> c_out: {out_channels}')
+            i += 1
+
+            if is_last_layer:
+                break
+            
+            
+    
 
 
     def forward(self, x):
-        x = self.deconv_1.forward(x)
-        x = F.relu(x)
-        x = self.deconv_2.forward(x)
+        x = self.seq(x)
         x = torch.sigmoid(x)
 
         return x
