b1617722a69d72a04719471e4fb8f577ea715856
maris <marisancans@gmail.com>
2019-06-02 10:53:41 +0300

    Refactored arguments

--- main.py
+++ main.py
@@ -12,88 +12,97 @@ from models import Encoder, Decoder
 def arg_to_bool(x): return str(x).lower() == 'true'
 parser = argparse.ArgumentParser()
 parser.add_argument('-debug', default=True, type=arg_to_bool)
+parser.add_argument('-device', default='cpu')
+parser.add_argument('-z_size', default=2, type=int)
+parser.add_argument('-image_size', default=63, type=int, help='Aviable sizes = 3, 7, 15, 31, 63, 127, 255, 511')
+parser.add_argument('-show', default=True, type=arg_to_bool)
 args = parser.parse_args()
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print('Using device:', DEVICE)
+print('Using device:', args.device)
 
-batch_size = 2
-
-# Aviable sizes
-# [3 3] [7 7] [15 15] [31 31] [63 63] [127 127] [255 255] [511 511]
-WIDTH = 31
-HEIGHT = 31
-
-encoder = Encoder().to(DEVICE)
-decoder = Decoder(HEIGHT, WIDTH, encoder.out_size).to(DEVICE)
+encoder = Encoder(args).to(args.device)
+decoder = Decoder(args).to(args.device)
 
 def rnd_color():
-    f = lambda : random.random()
+    f = lambda : random.randint(0, 255)
     return f(), f(), f()
 
-def create_image(id=0):
-    r, g, b = rnd_color()
-        
-    img = np.ones((HEIGHT, WIDTH, 3), np.uint8)
-    img[:] = r, 0, 0
+def create_images(size, buffer_size):
+    buffer = []
 
-    radius = random.randint(int(WIDTH/3), int(WIDTH/2))
-    cv2.circle(img, (int(HEIGHT/2), int(WIDTH/2)), 5, (255, 0, 0), -1)
+    for nth_img in range(buffer_size):
+        r, g, b = rnd_color()
+            
+        img = np.ones((size, size, 3), np.uint8)
+        img[:] = 255, 0, 0
+
+        radius = random.randint(int(size/5), int(size/2))
+        rnd_x = random.randint(0, size)
+        cv2.circle(img, (int(size/2), rnd_x), radius, (0, 0, r), -1)
+
+        buffer.append(img)
+
+        if (nth_img + 1)  % (buffer_size // 10) == 0:
+            print(f'{((nth_img + 1)/buffer_size) * 100}%')
     
-    return img
-
-def make_batch_multithreaded(batch_size):   
-    pool = multiprocessing.Pool(processes = multiprocessing.cpu_count())
-    batch = pool.map(create_image, range(5))
-
-    return batch
-
-def make_batch_debug(batch_size):
-    return [create_image() for _ in range(batch_size)]
+    return buffer
 
 
 
 
 ###### PARAMS
 LR = 0.01
+batch_size = 32
+buffer_size = 1000
+
 params = list(encoder.parameters()) + list(decoder.parameters())
 optimizer = torch.optim.Adam(params, LR)
-loss_fn = torch.nn.MSELoss()
+loss_fn = torch.nn.L1Loss()
 
 def show(truth_t, pred_t):
-    img = torch.cat((truth_t, pred_t), dim=2)
-    img = img.permute(1, 2, 0)
-    img = img.cpu().detach().numpy()
-
-    img = truth_t
-
+    img_t = torch.cat((truth_t, pred_t), dim=2)
+    img_t = img_t.permute(1, 2, 0)
+   
+    img = img_t.cpu().detach().numpy()
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.imshow('image', img)
     cv2.waitKey(1)
 
-for _ in range(100000):
-    if args.debug:
-        batch = make_batch_debug(batch_size)
-    else:
-        batch = make_batch_multithreaded(batch_size)
+def save(encoder, decoder):
+    torch.save()
 
-    batch_t = torch.FloatTensor(batch).to(DEVICE)
-    batch_t = batch_t.permute(0, 3, 1, 2) # (B, H, W, C) -->  (B, C, H, W)
 
-    z_vector = encoder.forward(batch_t)
-    pred_t = decoder.forward(z_vector)
-    loss = loss_fn(pred_t, batch_t)
+print(f'Generating samples...')
+buffer = create_images(args.image_size, buffer_size)
+
+print('Movng to device...')
+buffer_t = torch.FloatTensor(buffer).to(args.device)
+buffer_t = buffer_t.permute(0, 3, 1, 2) # (B, H, W, C) -->  (B, C, H, W)
+
+
+for i in range(1000000):
+    rnd_idxs = random.sample(range(0, len(buffer_t)), batch_size)
+    batch_t = buffer_t[rnd_idxs]
+
+    z_vector, z_mu, z_sigma = encoder.forward(batch_t)
+    decoded = decoder.forward(z_vector)
+    
+    loss_recunstruction = loss_fn(decoded, batch_t)
+
+    loss_kl = 250 * 0.5 * torch.mean(1.0 + torch.log(z_sigma**2) - z_mu**2 - z_sigma**2)
+    loss = loss_recunstruction + loss_kl
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    print(float(loss))
+    print(f'{float(loss_recunstruction):.3}  {float(loss_kl):.3}')
+    if args.show:
+        show(batch_t[0], decoded[0])
 
-    truth = batch_t[0]
-    pred = pred_t[0]
+    
 
-    show(truth, pred)
+
 
 
     x = 1
--- models.py
+++ models.py
@@ -4,32 +4,55 @@ import torchvision.models as models
 import torch.nn as nn
 
 class Encoder(torch.nn.Module):
-    def __init__(self):    
+    def __init__(self, args):    
         super().__init__()
 
+        self.init_densenet()
+        self.args = args
+       
+        self.mu = torch.nn.Linear(in_features=self.densenet_out, out_features=self.args.z_size)
+        self.sigma = torch.nn.Linear(in_features=self.densenet_out, out_features=self.args.z_size)
+
+    def init_densenet(self):
         pretrained_model = models.densenet161(pretrained=True)
         features_pretrained = next(iter(pretrained_model.children()))
 
-        self.encoder = torch.nn.Sequential()
+        self.seq = torch.nn.Sequential()
         
         for name, module in features_pretrained.named_children():
             if name == 'norm5':
-                self.out_size = module.num_features
-            self.encoder.add_module(name, module)
+                self.densenet_out = module.num_features
+            self.seq.add_module(name, module)
 
-        self.encoder.add_module('avg_pool', torch.nn.AdaptiveAvgPool2d(output_size=1))
+        self.seq.add_module('avg_pool', torch.nn.AdaptiveAvgPool2d(output_size=1))
 
-        # self.encoder_out = torch.nn.Linear(in_features=self.out_size, out_features=self.args.conv_encoder_layer_out)
 
     def forward(self, x):
-        x = self.encoder(x)
+        x = self.seq(x)
+        bs = x.shape[0]
+        features = x.shape[1]
+        x = x.view(bs, features)
+       
 
-        return x
+        # VAE
+        z_mu = self.mu.forward(x)
+        z_sigma = self.sigma.forward(x)
+        eps = torch.randn(bs, self.args.z_size).to(self.args.device) * z_sigma + z_mu # Sampling epsilon from normal distributions
+        
+        
+        z_vector = z_mu + z_sigma * eps # z ~ Q(z|X)
+        return z_vector, z_mu, z_sigma
 
     
 class Decoder(torch.nn.Module):
-    def __init__(self, orig_height, orig_width, in_channels):    
+    def __init__(self, args):    
         super().__init__()
+
+        self.args = args
+        in_channels = self.args.z_size
+
+        orig_height = self.args.image_size
+        orig_width = self.args.image_size
 
         h_out = 0
         w_out = 0
@@ -54,25 +77,31 @@ class Decoder(torch.nn.Module):
             if is_last_layer:
                 out_channels = 3
             else:
-                out_channels = int(in_channels // 2)
+                if i == 0:
+                    out_channels = in_channels * 100 // 2
+                else:
+                    out_channels = in_channels // 2
+                
 
             self.seq.add_module(f'deconv_{i}', nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel, stride=stride))
+            
+            
             in_channels = out_channels
 
             if not is_last_layer:
                 self.seq.add_module(f'relu_{i}', nn.ReLU())
+                self.seq.add_module (f'bn_{i}', nn.BatchNorm2d(num_features=in_channels))
 
             print(f'layer: {i} | h_out: {h_out} | w_out: {h_out} | c_in: {in_channels} --> c_out: {out_channels}')
-            i += 1
 
             if is_last_layer:
                 break
+
+            i += 1
             
             
-    
-
-
     def forward(self, x):
+        x = x.view(x.shape[0], x.shape[1], 1, 1)
         x = self.seq(x)
         x = torch.sigmoid(x)
 
